613a22df0b152a433ac47d37377340756c8d4e66
Abylay Kuvatov <abukuvatov@icloud.com>
2025-02-07 13:58:19 +0500

    Added test for getting metrics

--- molecule/default/tests/test_node_exporter.py
+++ molecule/default/tests/test_node_exporter.py
@@ -7,3 +7,10 @@ def test_node_exporter_service(host):
     service = host.service("node_exporter")
     assert service.is_running
     assert service.is_enabled
+
+
+def test_get_node_exporter_metrics_success(host):
+    cmd = host.run(
+        "curl -o /dev/null -s -w '%{http_code}' http://localhost:9100/metrics"
+    )
+    assert cmd.stdout == "200"
