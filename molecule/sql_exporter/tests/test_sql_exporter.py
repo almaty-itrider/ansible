4ddc87d2c158bc38d99ea462249d3b421d73c703
Abylay Kuvatov <abukuvatov@icloud.com>
2025-02-11 15:59:50 +0500

    Added ubuntu image for molecule test

--- molecule/sql_exporter/tests/test_sql_exporter.py
+++ molecule/sql_exporter/tests/test_sql_exporter.py
@@ -11,6 +11,6 @@ def test_sql_exporter_service(host):
 
 def test_get_sql_exporter_metrics_success(host):
     cmd = host.run(
-        "curl -o /dev/null -s -w '%{http_code}' http://localhost:9100/metrics"
+        "curl -o /dev/null -s -w '%{http_code}' http://localhost:9399/metrics"
     )
     assert cmd.stdout == "200"
